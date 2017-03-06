772dcb6664c2d2bbcbc3b91684af7951c43043fd
robote <2233628310@qq.com>
2017-03-06 17:50:14 +0800

    implement  post's  edit&destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit,  :update,  :destroy]
 
    def new
      @group = Group.find(params[:group_id])
@@ -20,6 +20,32 @@ class PostsController < ApplicationController
      end
    end
 
+   def  edit
+     @group = Group.find(params[:group_id])
+     @post = Post.find(params[:id])
+     @post.group  =  @group
+   end
+
+   def  update
+     @group  = Group.find(params[:group_id])
+     @post  =  Post.find(params[:id])
+     @post.group  =  @group
+     @post.user =  current_user
+
+     if  @post.update(post_params)
+       redirect_to  account_posts_path, notice: "编辑成功"
+     else
+       render :edit
+     end
+    end
+
+   def  destroy
+     @group = Group.find(params[:group_id])
+     @post = Post.find(params[:id])
+     @post.group  =  @group
+     @post.destroy
+     redirect_to  account_posts_path, alert:  "Post  deleted"
+   end
 
    private
 
